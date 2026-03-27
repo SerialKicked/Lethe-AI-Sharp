92f0e588fb9f40c3ccab2eafd4fd7ab149f2ab66
Anarkis Gaming <serialkicked@gmail.com>
2026-03-27 18:30:42 +0100

    Preliminary work for custom moodlets.

---
+++ Personas/IMoodlet.cs
@@ -0,0 +1,14 @@
+﻿namespace LetheAISharp.LLM
+{
+    public interface IMoodlet
+    {
+        string Name { get; set; }
+        double NeutralDecayRate { get; set; }
+        double NeutralValue { get; set; }
+        List<string> Triggers { get; set; }
+
+        string GetAdjective(double value);
+        double InterpretMessage(double currentValue, string message);
+        double OnTimePassed(double currentValue, TimeSpan timeSpan);
+    }
+}
---
+++ Personas/Moodlet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetheAISharp.LLM
+{
+    public class Moodlet : IMoodlet
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<string> Triggers { get; set; } = [];
+        public double NeutralValue { get; set; } = 0.5;
+        public double NeutralDecayRate { get; set; } = 0.005;
+
+        public double OnTimePassed(double currentValue, TimeSpan timeSpan)
+        {
+            return currentValue;
+        }
+
+        public double InterpretMessage(double currentValue, string message)
+        {
+            return currentValue;
+        }
+
+        public string GetAdjective(double value)
+        {
+            return string.Empty;
+        }
+    }
+}
